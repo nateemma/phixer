1c417cbd3952a866c2a4e917e84055f87fbbd903
Nateemma <philprice95@gmail.com>
2016-09-22 10:12:51 -0700

    Initial Commit

---
+++ FilterCam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FilterCam
+//
+//  Created by Philip Price on 9/22/16.
+//  Copyright © 2016 Nateemma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
